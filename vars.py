7549888b98ae563f563a4e29c70101871e0c4fd0
contentrajabot <109162597+contentrajabot@users.noreply.github.com>
2025-05-25 19:57:41 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,12 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "11335251"))
+API_HASH = environ.get("API_HASH", "39b4e23f71dcdd6138067fa7ff4656c9")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+OWNER = int(environ.get("OWNER", "5273580528"))
+CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
